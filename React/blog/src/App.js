13b9cc62b027ee7ff1754e0dbf484d9d2f2fc832
zzgh06 <zzgh06@naver.com>
2024-03-01 19:49:55 +0900

    240301-codingapple

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -18,6 +18,7 @@ function App() {
 
   let [글제목, 제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
   let [좋아요, 좋아요변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -33,15 +34,13 @@ function App() {
       {/* <h4>{ post }</h4> */}
       <button onClick={ () => {
           let titleCopy = [...글제목];
-          titleCopy.sort((a, b) => {
-            return ( a < b ) ? -1 : ( a == b ) ? 0 : 1; 
-          })
+          titleCopy.sort()
           제목변경(titleCopy);
         }}>제목순 정렬</button>  
       <div className='list'>
         {/* state 변경하는 법 : 등호로 변경금지, state변경함수(새로운 state) */}
         <h4>{ 글제목[0] } <span onClick={ () => { 좋아요변경(좋아요+1) }}>👍</span> { 좋아요 } </h4>
-        <button onClick={ () => { 
+        <button onClick ={ () => { 
           // array/object 다룰 때는 원본은 보존하는게 좋음
           // state 변경함수 특징 : 기존 state == 신규 state 의 경우 변경안해줌.
           let copy = [...글제목];
@@ -55,11 +54,63 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{ 글제목[2] } </h4>
+        <h4 onClick={ () => { setModal(!modal) } }>{ 글제목[2] } </h4>
+        {
+          modal == true ? <Modal/> : null
+        }
         <p>2월 17일 발행</p>
       </div>
+      
+      {/* 컴포넌트 만드는 법 */}
+      {/* 1. function 만들고 */}
+      {/* 2. return() 안에 html 담기 */}
+      {/* <함수명></함수명> 쓰기 */}
+      {/* 참고 : <함수명></함수명> 대신 <함수명/> 형식도 가능 */}
+
+      {/* 어떤걸 컴포넌트로 만들면 좋은가 */}
+      {/* 1. 반복적인 html 축약할 때 */}
+      {/* 2. 큰 페이지들 */}
+      {/* 3. 자주 변경 되는 것들 */}
+
+
+      {/* html 중간에 조건문 쓰려면 "삼항연산자" 사용 */}
+      {/* 조건식 ? 참일때 실행할 코드 : 거짓일 때 실행할 코드 */}
+      {
+        modal == true ? <Modal/> : null
+      }
+
+
     </div>
   );
 }
+
+// (참고1)
+// return() 안에 html 병렬 기입하려면
+// 의미없는 <div> 대신 <></> 사용가능
+function Modal(){
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
+
+// 컴포넌트 만드는 문법 2
+// let Modal = () => {
+//   return (
+//     <div className='modal'>
+//       <h4>제목</h4>
+//       <p>날짜</p>
+//       <p>상세내용</p>
+//     </div>
+//   )
+// }
+
+// 동적인 UI 만드는 step
+// 1. html css로 미리 디자인 완성
+// 2. UI의 현재 상태를 state로 저장
+// 3. state에 따라 UI가 어떻게 보일지 작성
 
 export default App;
